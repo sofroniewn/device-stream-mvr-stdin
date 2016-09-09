986add5341a3a258f4c4e3a45ae367dff507557e
Nicholas Sofroniew <sofroniewn@gmail.com>
2016-09-09 00:14:24 -0400

    update name

--- index.js
+++ index.js
@@ -11,11 +11,13 @@ module.exports = function () {
   var tty = TTY()
   var keymap = ['<left>', '<right>', '<up>', '<down>']
   var translationMap = [[-1, 1, 0, 0], [0, 0, 1, -1]]
-  var velocityForward = 0
-  var velocityLateral = 0
   var friction = 0.1
-  var response = false
   var started = false
+  var results = {
+    velocityForward: 0,
+    velocityLateral: 0,
+    response: false
+  }
 
   return {
     createStream: function () {
@@ -25,7 +27,7 @@ module.exports = function () {
         } else {
           console.log('Reward: ' + chalk.bgBlack('  '))        
         }
-        console.log('Trial number: ' + data.trialNumber)
+        console.log('Trial number: ' + data.trial)
         console.log('Left wall distance: ' + data.wallLeft + ' mm')
         console.log('Right wall distance: ' + data.wallRight + ' mm')
         console.log('Forward wall distance: ' + data.wallForward + ' mm')
@@ -39,19 +41,15 @@ module.exports = function () {
         timer.setInterval(function () {
           keymap.forEach(function (key, i) {
             if (tty.keysDown[key]) {
-              velocityLateral += 4/10*translationMap[0][i]
-              velocityForward += 4/10*translationMap[1][i]
+              results.velocityLateral += 4/10*translationMap[0][i]
+              results.velocityForward += 4/10*translationMap[1][i]
             }
           })
-          velocityLateral *= friction
-          velocityForward *= friction
-          if (tty.keysDown['R']) response = true
-          else response = false
-          readableStream.push({
-            velocityForward: velocityForward,
-            velocityLateral: velocityLateral,
-            response: response
-          })
+          results.velocityLateral *= friction
+          results.velocityForward *= friction
+          if (tty.keysDown['R']) results.response = true
+          else results.response = false
+          readableStream.push(results)
         }, '', '2m')
         started = true
       }
